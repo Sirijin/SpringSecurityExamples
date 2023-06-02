8aa8a755a70c7760ca76e60d8ecedefe47f1a3ae
Sirijin <pochta254@gmail.com>
2023-06-02 23:02:46 +0700

    add custom login page, and now we manually handle login and logout

--- springsecuritydemo/src/main/java/ru/mokrovitskii/springsecuritydemo/config/SecurityConfig.java
+++ springsecuritydemo/src/main/java/ru/mokrovitskii/springsecuritydemo/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package ru.mokrovitskii.springsecuritydemo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,6 +12,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import ru.mokrovitskii.springsecuritydemo.model.Role;
 
 @Configuration
@@ -24,8 +24,24 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
-                .httpBasic(Customizer.withDefaults())
-                .formLogin(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                        (auth) ->
+                                auth
+                                        .requestMatchers("/").permitAll()
+                                        .anyRequest().authenticated())
+                .formLogin(
+                        (login) ->
+                                login
+                                        .loginPage("/auth/login").permitAll()
+                                        .defaultSuccessUrl("/auth/success"))
+                .logout(
+                        (logout) ->
+                                logout
+                                        .logoutRequestMatcher(new AntPathRequestMatcher("/auth/logout", "POST"))
+                                        .invalidateHttpSession(true)
+                                        .clearAuthentication(true)
+                                        .deleteCookies("JSESSIONID")
+                                        .logoutSuccessUrl("/auth/login"))
                 .build();
     }
 
