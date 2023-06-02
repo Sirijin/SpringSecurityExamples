53ebfb316df6f13df1586bf058b89e408858168e
Sirijin <pochta254@gmail.com>
2023-06-02 20:03:41 +0700

    add role model to give access to endpoints by users role

--- springsecuritydemo/src/main/java/ru/mokrovitskii/springsecuritydemo/config/SecurityConfig.java
+++ springsecuritydemo/src/main/java/ru/mokrovitskii/springsecuritydemo/config/SecurityConfig.java
@@ -2,14 +2,18 @@ package ru.mokrovitskii.springsecuritydemo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import ru.mokrovitskii.springsecuritydemo.model.Role;
 
 @Configuration
 @EnableWebSecurity
@@ -17,7 +21,18 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http.build();
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                        (auth) ->
+                                auth
+                                        .requestMatchers(HttpMethod.GET, "/api/**").hasAnyRole(Role.ADMIN.name(), Role.USER.name())
+                                        .requestMatchers(HttpMethod.POST, "/api/**").hasAnyRole(Role.ADMIN.name())
+                                        .requestMatchers(HttpMethod.DELETE, "/api/**").hasAnyRole(Role.ADMIN.name())
+                                        .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(AbstractHttpConfigurer::disable)
+                .build();
     }
 
     @Bean
@@ -26,7 +41,12 @@ public class SecurityConfig {
                 User.builder()
                         .username("admin")
                         .password(passwordEncoder().encode("admin"))
-                        .roles("ADMIN")
+                        .roles(Role.ADMIN.name())
+                        .build(),
+                User.builder()
+                        .username("user")
+                        .password(passwordEncoder().encode("user"))
+                        .roles(Role.USER.name())
                         .build());
     }
 
---
+++ springsecuritydemo/src/main/java/ru/mokrovitskii/springsecuritydemo/model/Role.java
@@ -0,0 +1,6 @@
+package ru.mokrovitskii.springsecuritydemo.model;
+
+public enum Role {
+    USER,
+    ADMIN;
+}
